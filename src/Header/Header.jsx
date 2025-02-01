8256ea5dc59115d0ae113aa6ddac903f20b10e4c
anikatasnin <jarintasnin09@gmail.com>
2025-02-01 10:03:13 +0600

    Updated Header.jsx

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -22,32 +22,20 @@ const Header = () => {
             <ul
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-              <li><a>Item 1</a></li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </li>
-              <li><a>Item 3</a></li>
+              <li><a>Home</a></li>
+              <li><a>Recipes</a></li>
+              <li><a>About</a></li>
+              <li><a>Search</a></li>
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">Recipe Calories</a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </details>
-            </li>
-            <li><a>Item 3</a></li>
+              <li><a>Home</a></li>
+              <li><a>Recipes</a></li>
+              <li><a>About</a></li>
+              <li><a>Search</a></li>
           </ul>
         </div>
         <div className="navbar-end gap-2">
