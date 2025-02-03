7b266fb6c1a7cbc08d5046f98be73545e62ac394
anikatasnin <jarintasnin09@gmail.com>
2025-02-03 14:37:37 +0600

    updated recipes.jsx

--- src/Components/Recipes/Recipes.jsx
+++ src/Components/Recipes/Recipes.jsx
@@ -1,8 +1,9 @@
 
 const Recipes = () => {
     return (
-        <div>
-            
+        <div className="my-24">
+            <h2 className="text-4xl font-semibold text-center mb-5">Our Recipes</h2>
+            <p className="text-lg text-center mx-auto w-[823px] h-[52px] text-[#150B2BB2]">Explore a curated collection of delicious and easy-to-follow recipes that cater to all tastes and dietary preferences. Whether you’re craving comfort food or something new, we’ve got you covered!</p>
         </div>
     );
 };
