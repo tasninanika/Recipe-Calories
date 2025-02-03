ef6f2f8fea6cb74130759891169de34a42218503
anikatasnin <jarintasnin09@gmail.com>
2025-02-03 14:16:41 +0600

    updated banner.jsx

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -13,12 +13,12 @@ const Banner = () => {
             <div className="hero-content text-neutral-content text-center">
                 <div className="">
                 <h1 className="w-[897px] h-[152px] mb-6 text-[58px] text-[#FFF] font-bold">Discover an exceptional cooking class tailored for you!</h1>
-                <p className=" w-[933px] h-[60px] mb-10 text-lg text-[#FFF]">
+                <p className=" w-[933px] h-[60px] pt-5 mb-5 text-lg text-[#FFF]">
                 Whether you are a beginner or an experienced chef, our cooking classes offer personalized instruction to suit your skills and interests. Learn how to prepare mouth-watering dishes with expert guidance in a fun, hands-on environment. Join us and discover the joy of cooking while enhancing your culinary skills!
                 </p>
-                <div>
-                <button className="btn rounded-full text-black bg-[#0BE58A] border-none px-[30px] text-lg py-[25px]">Explore Now</button>
-                <button className="btn rounded-full text-black bg-[#0BE58A] border-none px-[30px] text-lg py-[25px]">Explore Now</button>
+                <div className='pt-16'>
+                <button className="btn rounded-full text-black bg-[#0BE58A] border-none px-[30px] text-lg py-[25px] mr-[15px]">Explore Now</button>
+                <button className="btn rounded-full text-[#FFF] bg-none btn-outline border-white px-[30px] text-lg py-[25px] ml-[15px] hover:text-black">Our Feedback</button>         
                 </div>
                 </div>
             </div>
