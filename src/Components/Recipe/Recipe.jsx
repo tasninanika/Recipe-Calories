83ba5de94f98c229fd986f122489a15b37a88550
anikatasnin <jarintasnin09@gmail.com>
2025-02-03 15:03:56 +0600

    updated recipes.jsx

---
+++ src/Components/Recipe/Recipe.jsx
@@ -0,0 +1,10 @@
+
+const Recipe = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Recipe;
--- src/Components/Recipes/Recipes.jsx
+++ src/Components/Recipes/Recipes.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
+import Recipe from "../Recipe/Recipe";
+import { useEffect } from "react";
 
 const Recipes = () => {
+
+    const [recipes, setRecipes] = useState([]);
+
+    
+
     return (
         <div className="mx-10 my-24">
             <h2 className="text-4xl font-semibold text-center mb-5">Our Recipes</h2>
             <p className="text-lg text-center mx-auto w-[823px] h-[52px] text-[#150B2BB2] mb-5">Explore a curated collection of delicious and easy-to-follow recipes that cater to all tastes and dietary preferences. Whether you’re craving comfort food or something new, we’ve got you covered!</p>
-            
+            <Recipe></Recipe>
         </div>
     );
 };
