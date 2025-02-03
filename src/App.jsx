f0a70db2dd606db20174ba5cc850b5761a4ecb8b
anikatasnin <jarintasnin09@gmail.com>
2025-02-03 13:30:33 +0600

    updated banner.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Banner from "./Components/Banner/Banner"
 import Header from "./Components/Header/Header"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
     <Header></Header>
+    <Banner></Banner>
     </>
   )
 }
