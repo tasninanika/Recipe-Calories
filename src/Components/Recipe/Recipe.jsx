3803c3f2e2c6db51056a81f8624a0c4ab30cde4a
anikatasnin <jarintasnin09@gmail.com>
2025-02-03 17:03:34 +0600

    updated recipe.jsx

--- src/Components/Recipe/Recipe.jsx
+++ src/Components/Recipe/Recipe.jsx
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
 import { IoTimerOutline } from "react-icons/io5";
+import { TbFlame } from "react-icons/tb";
+
 
 
 const Recipe = ({recipe}) => {
@@ -9,7 +11,7 @@ const Recipe = ({recipe}) => {
 
     return (
         <div className=''>
-            <div className="card w-96 rounded-xl border-gray-300 border-1">
+            <div className="card w-96 h-[685px] rounded-xl border-gray-300 border-1">
                 <figure className="px-6 pt-6">
                     <img
                     src={recipe_image}
@@ -18,16 +20,23 @@ const Recipe = ({recipe}) => {
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title">{recipe_name}</h2>
-                    <p className='border-b border-gray-200'>{short_description}</p>
-                    <h4>Ingredients: {ingredients.length}</h4>
-                    <ul className="list-disc pl-5 text-gray-600 border-gray-200 border-b">
+                    <p className='border-b border-gray-200 pb-4'>{short_description}</p>
+                    <h4 className=''>Ingredients: {ingredients.length}</h4>
+                    <ul className="list-disc pl-5 text-gray-600 border-gray-200 border-b mb-2">
                         {ingredients.map((ingredient, index) => (
-                            <li key={index}>{ingredient}</li>
+                            <li className='mb-2' key={index}>{ingredient}</li>
                         ))}
                     </ul>
-                    <p><IoTimerOutline />{preparing_time}                    </p>
+                    <div className="flex items-center text-gray-600 mb-4">
+                        <p className="flex items-center gap-1">
+                            <IoTimerOutline size={20} /> {preparing_time}
+                        </p>
+                        <p className="flex items-center gap-1 -ml-30">
+                            <TbFlame size={20} /> {calories}
+                        </p>
+                    </div>
                     <div className="card-actions">
-                    <button className="btn btn-primary">Buy Now</button>
+                    <button className="btn rounded-full text-black bg-[#0BE58A] border-none px-[20px] text-lg py-[15px]">Want to cook</button>
                     </div>
                 </div>
             </div>
