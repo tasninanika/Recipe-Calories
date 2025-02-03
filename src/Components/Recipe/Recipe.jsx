13777cd8c49d2724eb963d6c90cfc29d75d24a89
anikatasnin <jarintasnin09@gmail.com>
2025-02-04 00:02:27 +0600

    updated recipe.jsx

--- src/Components/Recipe/Recipe.jsx
+++ src/Components/Recipe/Recipe.jsx
@@ -11,34 +11,35 @@ const Recipe = ({recipe}) => {
 
     return (
         <div className=''>
-            <div className="card w-96 h-[685px] rounded-xl border-gray-300 border-1">
-                <figure className="px-6 pt-6">
-                    <img
-                    src={recipe_image}
-                    alt="food"
-                    className="rounded-2xl" />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">{recipe_name}</h2>
-                    <p className='border-b border-gray-200 pb-4'>{short_description}</p>
-                    <h4 className=''>Ingredients: {ingredients.length}</h4>
-                    <ul className="list-disc pl-5 text-gray-600 border-gray-200 border-b mb-2">
-                        {ingredients.map((ingredient, index) => (
-                            <li className='mb-2' key={index}>{ingredient}</li>
-                        ))}
-                    </ul>
-                    <div className="flex items-center text-gray-600 mb-4">
-                        <p className="flex items-center gap-1">
-                            <IoTimerOutline size={20} /> {preparing_time}
-                        </p>
-                        <p className="flex items-center gap-1 -ml-30">
-                            <TbFlame size={20} /> {calories}
-                        </p>
-                    </div>
-                    <div className="card-actions">
+            <div className="w-full h-full rounded-xl border border-gray-300 bg-white">
+            <figure className="px-6 pt-6">
+                <img src={recipe_image} alt="food" className="rounded-2xl w-full" />
+            </figure>
+
+            <div className="p-6">
+                <h2 className="text-2xl font-semibold">{recipe_name}</h2>
+                <p className="text-gray-600 border-b border-gray-200 pb-4">{short_description}</p>
+
+                <h4 className="font-semibold mt-4">Ingredients: {ingredients.length}</h4>
+                <ul className="list-disc pl-5 text-gray-600 border-b border-gray-200 pb-4 mb-4">
+                {ingredients.map((ingredient, index) => (
+                    <li className='mb-1' key={index}>{ingredient}</li>
+                ))}
+                </ul>
+
+                <div className="flex items-center text-gray-600 gap-4 mb-6">
+                <p className="flex items-center gap-1">
+                    <IoTimerOutline size={20} /> {preparing_time}
+                </p>
+                <p className="flex items-center gap-1">
+                    <TbFlame size={20} /> {calories}
+                </p>
+                </div>
+
+                <div className="card-actions">
                     <button className="btn rounded-full text-black bg-[#0BE58A] border-none px-[20px] text-lg py-[15px]">Want to cook</button>
                     </div>
-                </div>
+            </div>
             </div>
         </div>
     );
