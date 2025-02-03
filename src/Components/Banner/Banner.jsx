6ac89498ac17f21fd737143e27635dcd8137e6bf
anikatasnin <jarintasnin09@gmail.com>
2025-02-03 14:00:41 +0600

    updated banner.jsx

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -13,7 +13,7 @@ const Banner = () => {
             <div className="hero-content text-neutral-content text-center">
                 <div className="">
                 <h1 className="w-[897px] h-[152px] mb-5 text-[58px] text-[#FFF] font-bold">Discover an exceptional cooking class tailored for you!</h1>
-                <p className=" w-[933px] h-[60px]mb-5">
+                <p className=" w-[933px] h-[60px]mb-5 text-lg text-[#FFF]">
                 Whether you're a beginner or an experienced chef, our cooking classes offer personalized instruction to suit your skills and interests. Learn how to prepare mouth-watering dishes with expert guidance in a fun, hands-on environment. Join us and discover the joy of cooking while enhancing your culinary skills!
                 </p>
                 <button className="btn btn-primary">Get Started</button>
