b93eedea378728bb246e21806e7467d1fdf74088
anikatasnin <jarintasnin09@gmail.com>
2025-02-03 16:19:57 +0600

    updated recipe.jsx

--- src/Components/Recipe/Recipe.jsx
+++ src/Components/Recipe/Recipe.jsx
@@ -7,15 +7,15 @@ const Recipe = ({recipe}) => {
 
     return (
         <div className=''>
-            <div className="card w-96 rounded-lg border-gray-300 border-1">
-                <figure className="px-10 pt-10">
+            <div className="card w-96 rounded-xl border-gray-300 border-1">
+                <figure className="px-6 pt-6">
                     <img
                     src={recipe_image}
                     alt="food"
-                    className="rounded-xl" />
+                    className="rounded-2xl" />
                 </figure>
-                <div className="card-body items-center text-center">
-                    <h2 className="card-title">Card Title</h2>
+                <div className="card-body text-center">
+                    <h2 className="card-title">{recipe_name}</h2>
                     <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
                     <div className="card-actions">
                     <button className="btn btn-primary">Buy Now</button>
