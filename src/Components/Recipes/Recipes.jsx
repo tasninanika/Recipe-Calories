8c9ede6c2b8723230d6d17c4626af836552356e4
anikatasnin <jarintasnin09@gmail.com>
2025-02-03 15:15:02 +0600

    updated recipes.jsx

--- src/Components/Recipes/Recipes.jsx
+++ src/Components/Recipes/Recipes.jsx
@@ -6,7 +6,7 @@ const Recipes = () => {
 
     const [recipes, setRecipes] = useState([]);
 
-    useState(()=>{
+    useEffect(()=>{
         fetch('recipes.json')
         .then(res=> res.json())
         .then(data=> setRecipes(data))
@@ -16,7 +16,17 @@ const Recipes = () => {
         <div className="mx-10 my-24">
             <h2 className="text-4xl font-semibold text-center mb-5">Our Recipes</h2>
             <p className="text-lg text-center mx-auto w-[823px] h-[52px] text-[#150B2BB2] mb-5">Explore a curated collection of delicious and easy-to-follow recipes that cater to all tastes and dietary preferences. Whether you’re craving comfort food or something new, we’ve got you covered!</p>
-            <Recipe></Recipe>
+            <div className="md:w-2/3 mt-10">
+                {
+                    recipes.map(recipe=>
+                        <Recipe 
+                        key={recipes.recipe_id} 
+                        recipe={recipe}
+                        >
+                        </Recipe>
+                    )
+                }
+            </div>
         </div>
     );
 };
