3d80a45dcf714df7b9edf76604161d4717728da4
anikatasnin <jarintasnin09@gmail.com>
2025-02-04 13:46:11 +0600

    updated want_to_cook.jsx

--- src/Components/Want_to_cook/Want_to_cook.jsx
+++ src/Components/Want_to_cook/Want_to_cook.jsx
@@ -5,16 +5,15 @@ const Want_to_cook = ({wantToCook}) => {
             <div className="card rounded-xl border border-gray-300 bg-white text-primary-content w-full">
                 <div className="card-body">
                     <h2 className="card-title text-black flex justify-center pb-3 text-3xl border-b border-gray-200">Want to cook: {wantToCook.length}</h2>
-                    <div className="grid grid-cols-3">
+                    <div className="grid grid-cols-4">
                         <p className="text-gray-500 font-semibold">Name</p>
                         <p className="text-gray-500 font-semibold">Time</p>
                         <p className="text-gray-500 font-semibold">Calories</p>
                     </div>
-                    <div className="">
-                        {wantToCook.map((recipe,index) => (
-                            <p className="text-gray-500 font-semibold" key={index}>{index+1}</p>
-                        ))}
-                        <p className="text-gray-500 font-semibold">Name</p>
+                    <div className="grid grid-cols-4">
+                        <p className="text-gray-500 font-semibold"> {wantToCook.map((recipe,index) => (
+                            <span className="text-gray-500 font-semibold -ml-5 mr-3" key={index}>{index+1}</span>
+                        ))} Name</p>
                         <p className="text-gray-500 font-semibold">Time</p>
                         <p className="text-gray-500 font-semibold">Calories</p>
                         <button className="btn">Buy Now</button>
