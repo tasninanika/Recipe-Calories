38936f11dbd997ac0e894c0047599aa48da8a4b5
anikatasnin <jarintasnin09@gmail.com>
2025-02-06 10:20:42 +0600

    updated want_to_cook.jsx

--- src/Components/Want_to_cook/Want_to_cook.jsx
+++ src/Components/Want_to_cook/Want_to_cook.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import Cooking from '../Cooking/Cooking';
 
 const Want_to_cook = ({wantToCook}) => {
 
@@ -36,7 +37,33 @@ const Want_to_cook = ({wantToCook}) => {
                         </button>
                     </div>
                 ))}
+
+                {/* cooking */}
+                <h2 className="card-title text-black flex justify-center pb-3 text-3xl border-b border-gray-200 mx-10">Want to cook: {wantToCook.length}</h2>
+                
+                <div className="grid grid-cols-4 text-gray-500 font-semibold font-fira -mb-3">
+                    <p>Name</p>
+                    <p>Time</p>
+                    <p>Calories</p>
                 </div>
+                </div>
+                <div className="mb-2">
+                {wantToCook.map((recipe, index) => (
+                    <div key={index} className="grid grid-cols-4 text-gray-500 py-2 items-center gap-4 bg-gray-50 px-7">
+                        <p>{recipe.recipe_name}</p>
+                        <p>{recipe.preparing_time}</p>
+                        <p>{recipe.calories}</p>
+                        
+                        <button className="btn rounded-4xl text-black bg-[#0BE58A] text-xs border-none py-[2px] px-[18px]" onClick={handleCurrentlyCooking}> 
+                            
+                            Preparing
+                        </button>
+                    </div>
+                ))}
+
+
+                </div>
+                
             </div>
 
         </div>
