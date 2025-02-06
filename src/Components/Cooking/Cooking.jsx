fa9317f06dd9df5d9385f4453d2285297b381d67
anikatasnin <jarintasnin09@gmail.com>
2025-02-06 12:10:13 +0600

    updated want_to_cook.jsx

--- src/Components/Cooking/Cooking.jsx
+++ src/Components/Cooking/Cooking.jsx
@@ -4,13 +4,16 @@ const Cooking = ({ cook, index }) => {
 
     const {recipe_name, preparing_time, calories} = cook;
 
+
     return (
-        <div className="grid grid-cols-4 text-gray-500 bg-gray-50 font-fira py-2 items-center px-6">
-    <p className="">{index+1}</p>
-    <p className="text-left">{recipe_name}</p>  
-    <p className="text-center">{preparing_time}</p>  
-    <p className="text-center">{calories}</p>  
-</div>
+        <div>
+            <div className="grid grid-cols-4 text-gray-500 bg-gray-50 font-fira py-2 items-center px-6">
+            <p className="">{index+1}</p>
+            <p className="text-left">{recipe_name}</p>  
+            <p className="text-center">{preparing_time}</p>  
+            <p className="text-center">{calories}</p>  
+            </div>
+        </div>
 
     );
 };
--- src/Components/Want_to_cook/Want_to_cook.jsx
+++ src/Components/Want_to_cook/Want_to_cook.jsx
@@ -11,6 +11,10 @@ const Want_to_cook = ({wantToCook}) => {
         setCooking(newCurrentkyCooking);
     }
 
+    const totalTime = cooking.reduce((sum, cook) => sum + parseInt(cook.preparing_time), 0);
+    const totalCalories = cooking.reduce((sum, cook) => sum + parseInt(cook.calories), 0);
+
+
     return (
         <div className="md:w-1/3 my-8">
             <div className="card rounded-xl border border-gray-300 bg-white text-primary-content w-full">
@@ -40,24 +44,27 @@ const Want_to_cook = ({wantToCook}) => {
                 {/* cooking */}
                 <h2 className="card-title text-black flex justify-center pb-3 text-3xl border-b border-gray-200 mx-10 mt-8 mb-4">Currently cooking: {cooking.length}</h2>
                 
-                <div className="grid grid-cols-4 text-gray-500 font-semibold font-fira py-2 items-center px-7">
-                    <p className='w-1'></p>
-                    <p className="text-left">Name</p>  
-                    <p className="text-center">Time</p>  
-                    <p className="text-center">Calories</p>  
+                    <div className="grid grid-cols-4 text-gray-500 font-semibold font-fira py-2 items-center px-7">
+                        <p className='w-1'></p>
+                        <p className="text-left">Name</p>  
+                        <p className="text-center">Time</p>  
+                        <p className="text-center">Calories</p>  
+                    </div>
                 </div>
-
+                <div className="mb-2">               
+                    {
+                    cooking.map((cook,idx) => <Cooking key={idx} cook={cook} index={idx}></Cooking>)
+                    }
                 </div>
-                <div className="mb-2">
-                
-                {
-                cooking.map((cook,idx) => <Cooking key={idx} cook={cook} index={idx}></Cooking>)
-            }
-
-                </div>
-                
+                {cooking.length > 0 && (
+                    <div className="grid grid-cols-4 px-6 items-center font-semibold text-gray-500 py-2 mt-2 gap-6">
+                        <p></p>  
+                        <p className=""></p>
+                        <p className="text-left">Total Time = {totalTime} minutes</p>
+                        <p className="text-left">Total Calories = {totalCalories} calories</p>
+                    </div>
+                )}               
             </div>
-
         </div>
     );
 };
