026b0eb8e94152c413939044217029ea66ef62a5
anikatasnin <jarintasnin09@gmail.com>
2025-02-06 11:03:19 +0600

    updated want_to_cook.jsx

--- src/Components/Cooking/Cooking.jsx
+++ src/Components/Cooking/Cooking.jsx
@@ -1,12 +1,13 @@
 
-const Cooking = ({cook}) => {
-    console.log(cook);
+const Cooking = ({ cook }) => {
+
+    const {recipe_name, preparing_time, calories} = cook;
 
     return (
-        <div>
-            
-            <div className="mb-2">
-            </div>
+        <div className="grid grid-cols-4 text-gray-500 py-2 items-center gap-4 bg-gray-50 px-7">
+            <p>{recipe_name}</p>
+            <p>{preparing_time}</p>
+            <p>{calories}</p>
         </div>
     );
 };
